f11c8787ad47e2e57043f6706d3c137f13e19e61
Philip Winnington <ftcpjw@yahoo.com>
2018-09-24 21:49:59 -0400

    Intergrated new and create actions for portfolios

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,7 @@
 class PagesController < ApplicationController
   def home
     @posts = Blog.all
-<<<<<<< HEAD
     @skills = Skill.all
-=======
->>>>>>> 0f835711b582194d5cc00c625ca471e698ec058a
   end
 
   def about
