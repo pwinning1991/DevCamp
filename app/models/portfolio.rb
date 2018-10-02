b6a6d3420f16dbada77f56889bcb631882c60456
Philip Winnington <ftcpjw@yahoo.com>
2018-10-01 20:15:38 -0400

    added defautls for profolio posts

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -7,4 +7,11 @@ class Portfolio < ApplicationRecord
   end
 
   scope :ruby_on_rails, -> {where(subtitle: "Ruby on Rails")}
+
+  after_initialize :set_defatuls
+
+  def set_defautls
+    self.main_image ||= "https://placeholdit.co//i/600x400"
+    self.thumb_image ||= "https://placeholdit.co//i/350x150"
+  end
 end
