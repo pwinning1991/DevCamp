04e54d30979f5917b47e112aba3f86584d15dea1
Philip Winnington <ftcpjw@yahoo.com>
2018-10-28 23:52:53 -0400

    added validation for name to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates_presence_of :name
 
   def first_name
     self.name.split.first
