42fb64eb6837a93bd6e313c72efcdbd5a15c5e5a
Philip Winnington <ftcpjw@yahoo.com>
2018-11-05 00:37:11 -0500

    Added collection bases partials

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,9 +37,9 @@ end
 puts "5 skills created"
 
 
-8.times do |port|
+8.times do |portfolio_item|
  Portfolio.create!(
-   title: "Portfolio title #{port}",
+   title: "Portfolio title #{portfolio_item}",
    subtitle: "Ruby on Rails" ,
    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
    main_image: "https://placeholdit.co//i/600x400",
@@ -47,13 +47,11 @@ puts "5 skills created"
  )
 end
 
-1.times do |port|
+1.times do |portfolio_item|
  Portfolio.create!(
-   title: "Portfolio title #{port}",
+   title: "Portfolio title #{portfolio_item}",
    subtitle: "Angular" ,
-   body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-   main_image: "https://placeholdit.co//i/600x400",
-   thumb_image: "https://placeholdit.co//i/350x150"
+   body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", main_image: "https://placeholdit.co//i/600x400", thumb_image: "https://placeholdit.co//i/350x150"
  )
 end
 
