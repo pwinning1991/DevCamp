110a4a9f78e2d0df2aa8c835cf6957d1a566a563
Philip Winnington <ftcpjw@yahoo.com>
2019-01-19 16:54:30 -0500

    Implemented initail desgins for portfolio index page

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,7 +6,7 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.page(params[:page]).per(5all)
+    @blogs = Blog.page(params[:page]).per(5)
     @page_title = "My Portfolio Blog"
   end
 
