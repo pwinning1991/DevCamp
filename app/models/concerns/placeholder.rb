55a264daa6d0ab23f7ef8b6bb5d4d4046b296049
Philip Winnington <ftcpjw@yahoo.com>
2019-01-24 21:22:49 -0500

    updated file upload forms and refactored placehodler into view

--- app/models/concerns/placeholder.rb
+++ app/models/concerns/placeholder.rb
@@ -1,7 +1,4 @@
 module Placeholder
   extend ActiveSupport::Concern
 
-  def self.image_generator(height:, width:)
-    "https://placeholdit.co//i/#{height}x#{width}"
-  end
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,10 +1,5 @@
 class Skill < ApplicationRecord
-  includes Placeholder
   validates_presence_of :title, :percent_utilized
 
-  after_initialize :set_defautls
 
-  def set_defautls
-    self.badge ||= Placeholder.image_generator(height:250, width:250)
-  end
 end
